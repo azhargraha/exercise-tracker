412fced02fb120751656775f8f6c24704f9778a1
Azhar Nugraha <azhargraha14@gmail.com>
2021-07-19 12:53:13 +0700

    exercise list done

--- src/components/ExercisesList.js
+++ src/components/ExercisesList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './Components.scss';
 
@@ -9,7 +10,29 @@ const ExercisesList = () => {
                 <h3>Exercise lists</h3>
                 <p>Here are the list of exercises you've been created.</p>
             </div>
-            
+            <div className="list-container">
+                <div className="log">
+                    <div className="upper-line row">
+                        <h3>Nama</h3>
+                        <Link to="/"></Link>
+                    </div>
+                    <div className="bottom-line row">
+                        <div id="desc">
+                            <p className="p-title">Description</p>
+                            <p>Exercise</p>
+                        </div>
+                        <div className="duration">
+                            <p className="p-title">Duration</p>
+                            <p>1h 5m</p>
+                        </div>
+                        <div className="date">
+                            <p className="p-title">Date</p>
+                            <p>21/02/2021</p>
+                        </div>
+                        <Link to="/">edit</Link>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 };
